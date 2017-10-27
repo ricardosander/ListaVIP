8d0d14813b720b6e7da20e1135cf2e9d338ebdbb
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-10-27 01:12:57 -0200

    Criando primeira rota.

--- src/main/java/br/com/ricardosander/listavip/Configuracao.java
+++ src/main/java/br/com/ricardosander/listavip/Configuracao.java
@@ -2,9 +2,19 @@ package br.com.ricardosander.listavip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class Configuracao {
+
+    @RequestMapping("/ola")
+    @ResponseBody
+    public String ola() {
+        return "Olá, bem vindo ao sistema lista VIPs.";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Configuracao.class, args);
