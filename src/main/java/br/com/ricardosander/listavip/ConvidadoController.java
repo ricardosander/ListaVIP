2ee5cb66f55f2986a82e0d87446ed780e8f2a076
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-10-27 02:18:39 -0200

    Usando repository, acesso ao banco via Hibernate/JPA e exibindo listagem em uma nova rota.

--- src/main/java/br/com/ricardosander/listavip/ConvidadoController.java
+++ src/main/java/br/com/ricardosander/listavip/ConvidadoController.java
@@ -1,13 +1,30 @@
 package br.com.ricardosander.listavip;
 
+import br.com.ricardosander.listavip.model.Convidado;
+import br.com.ricardosander.listavip.repository.ConvidadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ConvidadoController {
 
+    @Autowired
+    private ConvidadoRepository convidadoRepository;
+
     @RequestMapping("/")
     public String index() {
         return "index";
     }
+
+    @RequestMapping("/listaconvidados")
+    public String listaConvidados(Model model) {
+
+        Iterable<Convidado> convidados = convidadoRepository.findAll();
+        model.addAttribute("convidados", convidados);
+
+        return "listaconvidados";
+    }
+
 }
