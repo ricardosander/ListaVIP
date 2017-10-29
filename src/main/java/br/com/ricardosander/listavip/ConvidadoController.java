6ecec4db5b109b13ef279e6be60050894c6ea9eb
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-10-28 23:03:02 -0200

    Criando cadastro de convidado e utilizando spring boot devops.

--- src/main/java/br/com/ricardosander/listavip/ConvidadoController.java
+++ src/main/java/br/com/ricardosander/listavip/ConvidadoController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ConvidadoController {
@@ -22,6 +24,20 @@ public class ConvidadoController {
     public String listaConvidados(Model model) {
 
         Iterable<Convidado> convidados = convidadoRepository.findAll();
+        model.addAttribute("convidados", convidados);
+
+        return "listaconvidados";
+    }
+
+    @RequestMapping(value = "/salvar", method = RequestMethod.POST)
+    public String salvar(@RequestParam("nome") String nome, @RequestParam("email") String email, @RequestParam("telefone") String telefone, Model model) {
+
+        Convidado convidado = new Convidado(nome, email, telefone);
+
+        convidadoRepository.save(convidado);
+
+        Iterable<Convidado> convidados = convidadoRepository.findAll();
+
         model.addAttribute("convidados", convidados);
 
         return "listaconvidados";
