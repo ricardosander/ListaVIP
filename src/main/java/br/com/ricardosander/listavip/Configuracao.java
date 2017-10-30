67fa763dce206b810d60d855e544e9f4cc5b7349
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-10-30 01:40:23 -0200

    Removendo notação não necessária.

--- src/main/java/br/com/ricardosander/listavip/Configuracao.java
+++ src/main/java/br/com/ricardosander/listavip/Configuracao.java
@@ -2,13 +2,8 @@ package br.com.ricardosander.listavip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-
-
-
 
 @SpringBootApplication
-@Controller
 public class Configuracao {
 
     public static void main(String[] args) {
